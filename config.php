9ce9279a25148fc09807c933f7a25692d0f4fa9a
Rakshith R <r.rakshith0506@gmail.com>
2024-04-24 20:31:29 +0530

    Initial commit

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mariadb';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'skystymz_moodlms';
+$CFG->dbuser    = 'skystymz_moodlms';
+$CFG->dbpass    = '51]SuLp6OCg[[.]2';
+$CFG->prefix    = 'mdlsu_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => '',
+  'dbsocket' => '',
+  'dbcollation' => 'utf8mb4_general_ci',
+);
+
+$CFG->wwwroot   = 'http://lms.skyschool.co.in';
+$CFG->dataroot  = '/home/skystymz/moodlelms';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
